c1b798767af065848abe0023e7667b46b498835e
katerina-j <117080459+katerina-j@users.noreply.github.com>
2023-02-07 18:22:54 +0300

    Add files via upload

---
+++ application/config.php
@@ -0,0 +1,17 @@
+﻿<?php
+
+class DB
+{
+  //const DB_HOST = '127.0.0.1'; 
+  const DB_HOST = 'localhost';
+  const DB_USER = 'root';
+  const DB_PASSWORD = '';
+  const DB_NAME = 'mysite';
+  const CHARSET = 'utf8';
+  const DB_PREFIX = '';
+	
+  static private $db;
+  
+ 
+} 
+?>
