c2e448a6992e9e58ddd4c5aa6363c986c004ac35
katerina-j <117080459+katerina-j@users.noreply.github.com>
2023-02-08 21:12:27 +0300

    Update modelProfile.php

--- application/models/modelProfile.php
+++ application/models/modelProfile.php
@@ -6,9 +6,10 @@ class ModelProfile extends Model
 	public $name;
 	public $email;
 	public $password;
-	    public function getData()
-	    {	
-		    $user=file_get_contents('username.txt');
+	
+	public function getData()
+	{	
+	    $user=file_get_contents('username.txt');
             $data=parent::getPosts('users',$user);
             foreach($data as $row) {
 		        $id=$row['id'];
@@ -32,40 +33,41 @@ class ModelProfile extends Model
 	        }
 	        //echo $_POST['name2'];
 	       // echo $name;
-		}
-		//методы для добавления в бд
-		public function nameChange()
-		{
-			$user=file_get_contents('username.txt');
-			$params = array('name'=> $_POST['name2']);
-		    return parent::query ("UPDATE `users`
-  		    SET 
-		    `name` = :name
-  		    WHERE `username` = '$user'",
-		    $params);
-		}
-		
-		public function emailChange()
-		{
-			$user=file_get_contents('username.txt');
-			$params = array('email'=> $_POST['email2']);
-			return parent::query ("UPDATE `users`
-  		    SET 
-		    `email` = :email
-  		    WHERE `username` = '$user'",
-		    $params);	
-		}
-		
-		public function passwordChange()
-		{
-			$user=file_get_contents('username.txt');
-			$params = array('password'=> md5($_POST['password2']));
-			return parent::query ("UPDATE `users`
-  		    SET 
-		    `password` = :password
-  		    WHERE `username` = '$user'",
-		    $params);	
-		}
+        }
+	
+	//методы для добавления в бд
+	public function nameChange()
+	{
+		$user=file_get_contents('username.txt');
+		$params = array('name'=> $_POST['name2']);
+		return parent::query ("UPDATE `users`
+  		SET 
+		`name` = :name
+  		 WHERE `username` = '$user'",
+		 $params);
+	}
+	
+	public function emailChange()
+	{
+		$user=file_get_contents('username.txt');
+		$params = array('email'=> $_POST['email2']);
+		return parent::query ("UPDATE `users`
+  		SET 
+		`email` = :email
+  		WHERE `username` = '$user'",
+		$params);	
+	}
+	
+	public function passwordChange()
+	{
+		$user=file_get_contents('username.txt');
+		$params = array('password'=> md5($_POST['password2']));
+		return parent::query ("UPDATE `users`
+  		SET 
+		`password` = :password
+  		WHERE `username` = '$user'",
+	        $params);	
+	}
 		/*$args = [
 		'name' => $this->name,
 		'surname' =>  $this->name_2,
